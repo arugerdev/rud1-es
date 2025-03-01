2ed7dc1beb3a1966f0594b0c9ae9a78f9b25bd65
arugerdev <alvaroch250@gmail.com>
2025-03-01 11:06:58 +0100

    First commit

---
+++ src/layouts/default.tsx
@@ -0,0 +1,23 @@
+/* eslint-disable import/order */
+/* eslint-disable padding-line-between-statements */
+/* eslint-disable react/no-unknown-property */
+
+import { Link } from "@heroui/link";
+
+import { Navbar } from "@/components/navbar";
+
+export default function DefaultLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <div className="relative flex flex-col h-screen">
+      <Navbar />
+      <main className="container mx-auto max-w-7xl px-6 flex-grow pt-8">
+        {children}
+      </main>
+
+    </div>
+  );
+}
---
+++ src/pages/index.tsx
@@ -0,0 +1,858 @@
+/* eslint-disable import/order */
+/* eslint-disable padding-line-between-statements */
+/* eslint-disable react/no-unknown-property */
+// import { Link } from "@heroui/link";
+// import { Snippet } from "@heroui/snippet";
+// import { Code } from "@heroui/code";
+// import { button as buttonStyles } from "@heroui/theme";
+
+// import { siteConfig } from "@/config/site";
+// import { GithubIcon } from "@/components/icons";
+import { Canvas } from "@react-three/fiber";
+import { OrbitControls, useGLTF } from "@react-three/drei";
+import { motion, MotionValue, useScroll, useTransform } from "framer-motion";
+
+import DefaultLayout from "@/layouts/default";
+import { title, subtitle } from "@/components/primitives";
+import { useEffect, useState } from "react";
+
+function Model({
+  path,
+  position,
+  opacity = 1,
+}: {
+  path: string;
+  position: [number, number, number];
+  opacity?: number;
+}) {
+  const { scene } = useGLTF(path);
+
+  // Aplica la opacidad a todos los materiales del modelo
+  useEffect(() => {
+    scene.traverse((child) => {
+      if (child.isMesh) {
+        child.material.transparent = true;
+        child.material.opacity = opacity;
+      }
+    });
+  }, [scene, opacity]);
+
+  return (
+    <primitive
+      object={scene}
+      position={[position[0], position[1], position[2]]}
+      scale={1}
+    />
+  );
+}
+
+export default function IndexPage() {
+  const { scrollYProgress } = useScroll();
+  const [scrollValue, setScrollValue] = useState(0);
+
+  // Animación de separación en el eje Z
+  const titleAnimY = useTransform(scrollYProgress, [0, 0.09], [0, -100]);
+  const titleAnimOpacity = useTransform(scrollYProgress, [0, 0.05], [1, 0]);
+
+  const caseTopZ = useTransform(scrollYProgress, [0.05, 0.1, 0.2], [0, -40, -200]);
+  const caseTopOpacity = useTransform(scrollYProgress, [0.1, 0.2], [1, 0]);
+  const hatZ = useTransform(scrollYProgress, [0.05, 0.1, 0.2], [0, 0, -20]);
+  const rpiZ = useTransform(scrollYProgress, [0.05, 0.1], [0, 40]);
+  const caseBottomZ = useTransform(
+    scrollYProgress,
+    [0.05, 0.1, 0.2],
+    [0, 60, 200],
+  );
+  const caseBottomOpacity = useTransform(scrollYProgress, [0.1, 0.2], [1, 0]);
+  const caseBottomY = useTransform(scrollYProgress, [0.01, 0.05], [0, -30]);
+
+  useEffect(() => {
+    // Escuchar los cambios de scroll en tiempo real
+    const unsubscribe = scrollYProgress.onChange((value) => {
+      setScrollValue(value);
+    });
+
+    // Limpia el efecto al desmontar
+    return () => unsubscribe();
+  }, [scrollYProgress]);
+
+  return (
+    <DefaultLayout>
+      <section className="flex flex-col items-center justify-start gap-4 w-full h-full min-h-screen">
+        <motion.div
+          className="inline-block max-w-lg text-center justify-center"
+          style={{
+            transform: `translateY(${titleAnimY.get()}px)`,
+            opacity: `${titleAnimOpacity.get()}`,
+          }}
+        >
+          <span className={title()}>Conecta a tus dispositivos con&nbsp;</span>
+          <span className={title({ color: "violet" })}>Rud1&nbsp;</span>
+          <br />
+          <div className={subtitle({ class: "mt-4" })}>
+            Conexiones remotas rapidas y seguras con servicios VPN
+          </div>
+        </motion.div>
+        {/* 
+
+        <div className="flex gap-3">
+          <Link
+            isExternal
+            className={buttonStyles({
+              color: "primary",
+              radius: "full",
+              variant: "shadow",
+            })}
+            href={siteConfig.links.docs}
+          >
+            Documentation
+          </Link>
+          <Link
+            isExternal
+            className={buttonStyles({ variant: "bordered", radius: "full" })}
+            href={siteConfig.links.github}
+          >
+            <GithubIcon size={20} />
+            GitHub
+          </Link>
+        </div>
+
+        <div className="mt-8">
+          <Snippet hideCopyButton hideSymbol variant="bordered">
+            <span>
+              Get started by editing{" "}
+              <Code color="primary">pages/index.tsx</Code>
+            </span>
+          </Snippet>
+        </div> */}
+        <motion.div
+          className="w-full h-screen"
+          style={{
+            position: "fixed",
+            top: "50%",
+            left: "50%",
+            transform: "translate(-50%, -50%)",
+            pointerEvents: "none",
+            zIndex: -1,
+            width: "100%",
+          }}
+        >
+          <Canvas
+            camera={{ position: [0, 150, 0], fov: 70 }}
+            inputMode="none"
+            onInput={() => { }}
+          >
+            <ambientLight intensity={1} />
+            <directionalLight intensity={2} position={[10, 10, 5]} />
+            <motion.group>
+              <Model
+                opacity={caseTopOpacity.get()}
+                path="/models/caseTOP.glb"
+                position={[0, 0, caseTopZ.get()]}
+              />
+              <Model
+                opacity={1}
+                path="/models/rpi.glb"
+                position={[0, 0, rpiZ.get()]}
+              />
+              <Model
+                opacity={1}
+                path="/models/rpiHAT.glb"
+                position={[0, 0, hatZ.get()]}
+              />
+              <Model
+                opacity={caseBottomOpacity.get()}
+                path="/models/caseBOTTOM.glb"
+                position={[0, caseBottomY.get(), caseBottomZ.get()]}
+              />
+            </motion.group>
+
+            <OrbitControls
+              enableRotate={false}
+              enableZoom={false}
+              maxPolarAngle={Math.PI / 2}
+              target={[0, 0, 0]}
+            />
+          </Canvas>
+        </motion.div>
+
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+      </section>
+    </DefaultLayout>
+  );
+}
